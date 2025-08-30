4b532995424565e1727d8f271fa576e08cfc740e
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-08-31 01:58:26 +0300

    set up prettier

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import concurrently from 'concurrently';
 
 concurrently([
-    { command: 'cd packages/client && npm run dev', 
-        name: 'client', 
-        prefixColor: 'blue'
-     },
-    { command: 'cd packages/server && npm run dev', 
-        name: 'server', 
-        prefixColor: 'green' 
-    }
+   {
+      command: 'cd packages/client && npm run dev',
+      name: 'client',
+      prefixColor: 'blue',
+   },
+   {
+      command: 'cd packages/server && npm run dev',
+      name: 'server',
+      prefixColor: 'green',
+   },
 ]);
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,21 @@
-
-
-import { useEffect, useState } from "react"
-import { Button } from "./components/ui/button"
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('')
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-  }, [])
-  
-  return (
-    <div className="font-bold p-4 text-3xl">
-      <h1>{message}</h1>
-      <Button variant="outline"> Click Me</Button>
-    </div>
-  )
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setMessage(data.message));
+   }, []);
+
+   return (
+      <div className="font-bold p-4 text-3xl">
+         <h1>{message}</h1>
+         <Button variant="outline"> Click Me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
